333ee11d67876cadb7701a8b9636991ef0e8b6fd
Fabricio-Martins <52521053+Fabricio-Martins@users.noreply.github.com>
2020-11-04 22:25:18 -0300

    Add files via upload

---
+++ Blizzard Inserções.sql
@@ -0,0 +1,28 @@
+INSERT INTO A_ENGINE(nome, versao, api) VALUES ('Unity', 'Unity 2020', 'Direct3D');
+INSERT INTO A_ENGINE(nome, versao, api) VALUES ('GameMaker', 'GameMaker Studio 2', 'OpenGL');
+INSERT INTO A_ENGINE(nome, versao, api) VALUES ('UnrealEngine', 'Unreal Engine 4', 'Vulkan');
+INSERT INTO A_ENGINE(nome, versao, api) VALUES ('Frostbite', 'Frostbite 3', 'DirectX 11');
+
+INSERT INTO CONTEUDO_ADICIONAL	(
+	nome_microtransacao,
+    preco_microtransacao,
+    nome_dlc,
+    preco_dlc,
+    nome_jogo
+) VALUES ('Arma Lendária', 'R$ 15,00', 'The Burning Crusade', 'R$ 60,00', 'World of Warcraft');
+
+INSERT INTO CONTEUDO_ADICIONAL	(
+	nome_microtransacao,
+    preco_microtransacao,
+    nome_dlc,
+    preco_dlc,
+    nome_jogo
+) VALUES ('Skin', 'R$ 5,00', 'Wrath of the Lich King', 'R$ 90,00', 'World of Warcraft');
+
+INSERT INTO CONTEUDO_ADICIONAL	(
+	nome_microtransacao,
+    preco_microtransacao,
+    nome_dlc,
+    preco_dlc,
+    nome_jogo
+) VALUES ('Montaria', 'R$ 3,50', 'Mists of Pandaria', 'R$ 120,00', 'World of Warcraft');
